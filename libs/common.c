1a4bca655cad28906c41b2d149937a6f96fe867b
haiqiang0225 <haiqiang0225@gmail.com>
2021-12-29 15:10:39 +0800

    bugFix:端口读的代码编译总报错，改成现在的形式不报错了，暂不知原因

--- libs/common.c
+++ libs/common.c
@@ -1,18 +1,21 @@
 #include "common.h"
 
-
 // 端口写一个字节
 inline void outb(uint16_t port, uint8_t value)
 {
     // volatile用于告诉编译器，严禁将此处的汇编语句与其它的语句重组合优化，即：按代码原本的样子去处理这里的汇编。
-    asm volatile ("outb %1, %0" : : "dN" (port), "a" (value));
+    asm volatile("outb %1, %0"
+                 :
+                 : "dN"(port), "a"(value));
 }
 
 // 端口读一个字节
 inline uint8_t inb(uint16_t port)
 {
     uint8_t ret;
-    asm volatile("inb %1 %0" : "=a" (ret) : "dN" (port));
+    asm volatile("inb %1, %0"
+                 : "=a"(ret)
+                 : "dN"(port));
     return ret;
 }
 
@@ -20,6 +23,8 @@ inline uint8_t inb(uint16_t port)
 inline uint16_t inw(uint16_t port)
 {
     uint16_t ret;
-    asm volatile ("inw %1 %0" : "=a" (ret) : "dN" (port));
+    asm volatile("inw %1, %0"
+                 : "=a"(ret)
+                 : "dN"(port));
     return ret;
 }

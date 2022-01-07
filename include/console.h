85995275ed5211c785b1739bf6c6e6450e0c5125
haiqiang0225 <haiqiang0225@gmail.com>
2022-01-07 22:05:55 +0800

    Add: string.h&string.c 格式更改

--- include/console.h
+++ include/console.h
@@ -3,8 +3,7 @@
 
 #include "types.h"
 
-typedef
-enum read_color {
+typedef enum real_color {
     rc_black = 0,
     rc_blue = 1,
     rc_green = 2,
@@ -19,7 +18,7 @@ enum read_color {
     rc_light_cyan = 11,
     rc_light_red = 12,
     rc_light_magenta = 13,
-    rc_light_brown  = 14,   // yellow
+    rc_light_brown = 14, // yellow
     rc_white = 15
 } real_color_t;
 
@@ -30,10 +29,10 @@ void console_clear();
 void console_putc_color(char c, real_color_t back, real_color_t fore);
 
 // 屏幕打印一个以 \0 结尾的字符串，默认黑底白字
-void console_write(char *cstr);
+void console_write(char* cstr);
 
 // 屏幕打印一个以 \0 结尾的字符串，带颜色
-void console_write_color(char *cstr, real_color_t back, real_color_t fore);
+void console_write_color(char* cstr, real_color_t back, real_color_t fore);
 
 // 屏幕输出一个十六进制的整型数
 void console_write_hex(uint32_t n, real_color_t back, real_color_t fore);
---
+++ libs/string.c
@@ -0,0 +1,62 @@
+#include "string.h"
+
+inline void memcpy(uint8_t* dest, const uint8_t* src, uint32_t len)
+{
+    for (; len != 0; len--) {
+        *dest++ = *src++;
+    }
+}
+
+inline void memset(void* dest, uint8_t val, uint32_t len)
+{
+    uint8_t* dst = (uint8_t*)dest;
+
+    for (; len != 0; len--) {
+        *dst++ = val;
+    }
+}
+
+inline void bzero(void* dest, uint32_t len) { memset(dest, 0, len); }
+
+inline int strcmp(const char* str1, const char* str2)
+{
+    while (*str1 == *str2) {
+        str1++;
+        str2++;
+    }
+    return *str1 - *str2;
+}
+
+inline char* strcpy(char* dest, const char* src)
+{
+    char* ret = dest;
+    while (*src) {
+        *dest++ = *src++;
+    }
+
+    *dest = '\0';
+
+    return ret;
+}
+
+inline char* strcat(char* dest, const char* src)
+{
+    char* ret = dest;
+    while (*dest) {
+        dest++;
+    }
+
+    while ((*dest++ = *src++))
+        ;
+
+    return ret;
+}
+
+inline int strlen(const char* src)
+{
+    char* end = src;
+    while (*end++)
+        ;
+
+    return (end - src - 1);
+}

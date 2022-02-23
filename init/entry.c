0931fc2082d76e4d58e89c4c6e87cce9ba2105a5
haiqiang0225 <haiqiang0225@gmail.com>
2022-02-23 09:31:32 +0800

    add debug.c

--- init/entry.c
+++ init/entry.c
@@ -1,10 +1,15 @@
 #include "console.h"
+#include "debug.h"
 
 int kern_entry()
 {
+    init_debug();
+
     console_clear();
 
-    console_write_color("Hello, OS kernel!\n", rc_black, rc_green);
+    printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
+
+    panic("test");
 
     return 0;
 }

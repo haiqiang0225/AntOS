5830cd7b020b8b190aa82b269f164ab3533c6d59
haiqiang0225 <haiqiang0225@gmail.com>
2022-02-23 14:57:33 +0800

    add Global Descriptor Table，GDT

--- init/entry.c
+++ init/entry.c
@@ -1,10 +1,12 @@
 #include "console.h"
 #include "debug.h"
+#include "gdt.h"
 
 int kern_entry()
 {
     init_debug();
-
+    init_gdt();
+    
     console_clear();
 
     printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
